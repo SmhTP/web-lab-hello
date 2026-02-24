b683a8e91aef38c0e6e0c5b013cfe1ed57294639
Semih Tepe <semihtepe09@gmail.com>
2026-02-24 20:30:16 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="lab-container">
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB -1</h2>
+      <p>Ad Soyad: Semih Tepe</p>
+      <p>Ogrenci No: [235541052]</p>
+    </div>
+  );
+}
+
+export default App;
